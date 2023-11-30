1d7332977e36b9bd5b44f4f6f04501c4e8b0f2d5
ekiel311 <griswoldlegacy@yahoo.com>
2023-11-30 17:56:54 +0800

    simple semi functioning

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,4 +1,59 @@
+let set;
+let result;
+let display = document.querySelector("#calc-input");
+
 // Clear Button
 function clearInput() {
-    document.querySelector("#calc-input").value = "";
+    display.value = "";
+    set = "";
+}
+
+function calcBtn(numPress) {
+    if (display.value == 0) {
+        display.value = "";
+        display.value = numPress;
+        set = numPress;
+    } else {
+        display.value = display.value + numPress;
+        set = set + numPress;
+    }
+}
+
+function plus() {
+    display.value = display.value + "+";
+    set = set + "+";
+}
+
+function minus() {
+    display.value = display.value + "-";
+    set = set + "-";
+}
+
+function times() {
+    display.value = display.value + "x";
+    set = set + "*";
+}
+
+function dividedBy() {
+    display.value = display.value + "÷";
+    set = set + "/";
+}
+
+function equals() {
+    try {
+        result = eval(set);
+        if (isNaN(result) || !isFinite(result)) {
+            throw new Error("Invalid operation");
+        }
+        display.value = result;
+    } catch (error) {
+        if (error instanceof SyntaxError || error.message === "Invalid operation") {
+            display.value = "Error";
+        } else if (error instanceof TypeError && error.message === "Division by zero") {
+            display.value = "Cannot divide by 0";
+        } else {
+            console.error(error);
+            display.value = "Error";
+        }
+    }
 }
